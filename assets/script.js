8f0b3040d9bdb83cad1de52a4d0c020acb7d5ded
Jorge Garit <jorgegarit@gmail.com>
2022-06-30 00:13:32 -0400

    added the option to cancel when you hit generate

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,35 @@
 // Assignment code here
 
-var passwordOptions = []
-var randomNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var randomSpecial = ['!', '"', '#', '$', '%', '&', "'", ')', '(', '*', '+', ','];
-var randomUppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var randomLowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var noneSelected = ["If you weren't going to generate a password then why are you even here!"];
-
-
-// connect code to button in html
+// connects code to button in html
 var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-
-var randomInteger = function(min, max) {
-  if (!max) {
-    max = min
-    min = 0
-  }
-  var mathRand = Math.random()
-  return Math.floor(min * (1 - mathRand) + mathRand * max)
-}
-
-var getRandomChoice = function(list) {
-  return list[randomInteger(list.length)]
-}
-
-
 var generatePassword = function() {
-  var userInput = window.prompt("How long do you want the password to be? (Between 8 and 128 character)")
 
-  var characterLength = parseInt(userInput)
+  while (true) {
+  
+    var userInput = window.prompt("How long do you want the password to be? (Between 8 and 128 character)")
 
-  if (isNaN(characterLength)) {
-    window.alert("That is not a number! Please input a number between 8 and 128")
-
-    generatePassword();
-
-  } else if (characterLength < 8 || characterLength > 128) {
-      window.alert("Invalid password length, please input a number between 8 and 128")
-
-      generatePassword();
+    // if user clicks generate but decides to cancel
+    if (userInput === null) {
+      return "Your Secure Password";
     }
+
+    var characterLength = parseInt(userInput)
+
+    if (isNaN(characterLength)) {
+      window.alert("That is not a number! Please input a number between 8 and 128")
+
+    } else if (characterLength < 8 || characterLength > 128) {
+        window.alert("Invalid password length, please input a number between 8 and 128")
+
+    } else {
+      break;
+    }
+  }  
 
   var confirmNumbers = window.confirm("Would you like numbers in your password?");
   var confirmSpecial = window.confirm("Would you like special characters in your password?");
@@ -80,10 +65,32 @@ var generatePassword = function() {
     var randomCharacter = getRandomChoice(randomChoice)
     createdPassword = createdPassword + randomCharacter
   }
-  return createdPassword
+  return createdPassword //Sends createdPassword to password text box
 }
 
+// arrays created to later insert into for loop
+var passwordOptions = []
+var randomNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var randomSpecial = ['!', '"', '#', '$', '%', '&', "'", ')', '(', '*', '+', ','];
+var randomUppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var randomLowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var noneSelected = ["If you weren't going to generate a password then why are you even here!"];
 
+
+// Generates a random integer between max and min values (base on character length) to then later apply to list
+var randomInteger = function(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var mathRand = Math.random()
+  return Math.floor(min * (1 - mathRand) + mathRand * max)
+}
+
+// selects character from list that is asscoaited to generated integer
+var getRandomChoice = function(list) {
+  return list[randomInteger(list.length)]
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -91,7 +98,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 
