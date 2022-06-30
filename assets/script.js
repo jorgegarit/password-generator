3fb292bfb00941aeb03b2ab15006b3f66f30e32c
Jorge Garit <jorgegarit@gmail.com>
2022-06-29 23:41:53 -0400

    generated password and returned it to text box

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,30 @@ var randomNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var randomSpecial = ['!', '"', '#', '$', '%', '&', "'", ')', '(', '*', '+', ','];
 var randomUppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var randomLowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var noneSelected = ["If you weren't going to generate a password then why are you even here!"];
+
 
 // connect code to button in html
 var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+var randomInteger = function(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var mathRand = Math.random()
+  return Math.floor(min * (1 - mathRand) + mathRand * max)
+}
+
+var getRandomChoice = function(list) {
+  return list[randomInteger(list.length)]
+}
+
 
 var generatePassword = function() {
   var userInput = window.prompt("How long do you want the password to be? (Between 8 and 128 character)")
@@ -48,8 +66,21 @@ var generatePassword = function() {
   if (confirmLower === true) {
     passwordOptions.push(randomLowercase)
   }
+
+  if (passwordOptions.length === 0) {
+    passwordOptions.push(noneSelected)
+
+  }
+
   
-  console.log(passwordOptions);
+  var createdPassword = ""
+
+  for (var i = 0; i < characterLength; i++  ) {
+    var randomChoice = getRandomChoice(passwordOptions)
+    var randomCharacter = getRandomChoice(randomChoice)
+    createdPassword = createdPassword + randomCharacter
+  }
+  return createdPassword
 }
 
 
