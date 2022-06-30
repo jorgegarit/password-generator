44c74fb0ca7864e613a405f820c1717d6c0080fa
Jorge Garit <82242154+jorgegarit@users.noreply.github.com>
2022-06-30 10:23:33 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,6 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 function writePassword() {
-  passwordText.value = [];
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
